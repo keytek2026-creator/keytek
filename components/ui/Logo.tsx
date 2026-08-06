fbc854b8fb947750a923fe02b82071ec81609f68
Scott894139 <scott.axel.b@gmail.com>
2026-08-06 14:11:29 -0400

    style: increase logo image size

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -10,12 +10,12 @@ export function Logo({ className = "", light = false }: LogoProps) {
   return (
     <Link href="/" className={`flex items-center gap-2.5 ${className}`}>
       {/* Official Brand Logo Image */}
-      <div className="relative flex h-10 w-10 items-center justify-center">
+      <div className="relative flex h-14 w-14 items-center justify-center">
         <Image
           src="/Futuristic_Logo_with_Interlocking__K_-removebg-preview.png"
           alt="Keytek Logo"
-          width={40}
-          height={40}
+          width={56}
+          height={56}
           className="object-contain"
           priority
         />
