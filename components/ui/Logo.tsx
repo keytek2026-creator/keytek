65ec36cc111c910f39dcd05cda858c4855f01caf
Scott894139 <scott.axel.b@gmail.com>
2026-08-25 17:40:59 -0400

    style: fix logo crop coordinates to include full Vaultec brand text and update dark theme logo

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -10,11 +10,11 @@ export function Logo({ className = "", light = false }: LogoProps) {
   return (
     <Link href="/" className={`flex items-center ${className}`}>
       {/* Official Brand Logo Image scaled as a landscape rectangle */}
-      <div className="relative flex h-[55px] w-[130px] items-center justify-start shrink-0">
+      <div className="relative flex h-[55px] w-[150px] items-center justify-start shrink-0">
         <Image
           src={light ? "/logo_sinfondo_white.png" : "/logo_sinfondo.png"}
           alt="Keytek Logo"
-          width={130}
+          width={150}
           height={55}
           className="object-contain object-left"
           priority
