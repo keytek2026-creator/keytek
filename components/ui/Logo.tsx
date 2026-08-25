734da5f80e5a2fcca1c2369fb92c5267c34ed247
Scott894139 <scott.axel.b@gmail.com>
2026-08-25 17:58:09 -0400

    style: reduce logo container width to 140px in Logo component to prevent clipping on mobile viewports

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -10,11 +10,11 @@ export function Logo({ className = "", light = false }: LogoProps) {
   return (
     <Link href="/" className={`flex items-center ${className}`}>
       {/* Official Brand Logo Image scaled as a landscape rectangle */}
-      <div className="relative flex h-[55px] w-[180px] items-center justify-start shrink-0">
+      <div className="relative flex h-[55px] w-[140px] items-center justify-start shrink-0">
         <Image
-          src={light ? "/logo_sinfondo_white.png?v=3" : "/logo_sinfondo.png?v=3"}
+          src={light ? "/logo_sinfondo_white.png?v=4" : "/logo_sinfondo.png?v=4"}
           alt="Keytek Logo"
-          width={180}
+          width={140}
           height={55}
           className="object-contain object-left"
           priority
