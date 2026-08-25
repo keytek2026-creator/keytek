e1e01f5fdea25ef1e386b7a9dfe29692475c6aa3
Scott894139 <scott.axel.b@gmail.com>
2026-08-25 17:43:46 -0400

    style: fix crop offset to include the entire Vaultec word and expand logo container width to 180px

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -10,11 +10,11 @@ export function Logo({ className = "", light = false }: LogoProps) {
   return (
     <Link href="/" className={`flex items-center ${className}`}>
       {/* Official Brand Logo Image scaled as a landscape rectangle */}
-      <div className="relative flex h-[55px] w-[150px] items-center justify-start shrink-0">
+      <div className="relative flex h-[55px] w-[180px] items-center justify-start shrink-0">
         <Image
           src={light ? "/logo_sinfondo_white.png" : "/logo_sinfondo.png"}
           alt="Keytek Logo"
-          width={150}
+          width={180}
           height={55}
           className="object-contain object-left"
           priority
