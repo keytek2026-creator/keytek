bdfefab6fe18f1f42f7193ab80e08d543e43c45b
Scott894139 <scott.axel.b@gmail.com>
2026-08-25 17:51:18 -0400

    style: force browser cache reload for new logos using URL versioning parameters

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -12,7 +12,7 @@ export function Logo({ className = "", light = false }: LogoProps) {
       {/* Official Brand Logo Image scaled as a landscape rectangle */}
       <div className="relative flex h-[55px] w-[180px] items-center justify-start shrink-0">
         <Image
-          src={light ? "/logo_sinfondo_white.png" : "/logo_sinfondo.png"}
+          src={light ? "/logo_sinfondo_white.png?v=3" : "/logo_sinfondo.png?v=3"}
           alt="Keytek Logo"
           width={180}
           height={55}
